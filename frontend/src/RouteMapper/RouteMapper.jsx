27430ff500aeba7b294ba4f091184e9efa3102d5
Arjun Verma <arjun.holmie@gmail.com>
2024-10-03 12:41:22 +0530

    removed fe

--- frontend/src/RouteMapper/RouteMapper.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import WithLogin from '../WithLogin/WithLogin'
-
-export default function RouteMapper({children}) {
-  return (
-    children.map(route=><WithLogin>{route}</WithLogin>)
-  )
-}
--- frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from '../features/users/userSlice'
-
-export default configureStore({
-  reducer: {
-    user: userReducer
-  },
-})
